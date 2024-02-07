5eb4a81d98956df7bf8d4bd629ad54aad4d66a7e
Eiley2 <erikmauriethsanchez_1336@hotmail.es>
2024-02-06 20:59:42 -0500

    dasd

--- drizzle-orm/src/errors.ts
+++ drizzle-orm/src/errors.ts
@@ -3,7 +3,7 @@ import { entityKind } from '~/entity.ts';
 export class DrizzleError extends Error {
 	static readonly [entityKind]: string = 'DrizzleError';
 
-	constructor({ message, cause }: { message?: string; cause?: unknown }) {
+	constructor({ message, cause } { message?: string; cause?: unknown }) {
 		super(message);
 		this.name = 'DrizzleError';
 		this.cause = cause;
